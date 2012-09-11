55d88161210a9712901d871935e6ef82b7c5eea4
António Cascalheira <cascalheira@gmail.com>
2012-09-11 18:34:51 +0100

    Added a close button. Now the window will stay open after running something.

--- init.js
+++ init.js
@@ -4,11 +4,14 @@ Hooks.addMenuItem("Go/Utils/Node Executer", "cmd-ctrl-n", function() {
     var win = new Window();
     win.title = "Node Executer"
     win.html = "<TEXTAREA id='nodeCMD' style='width:100%;height:100%' ROWS=40>";
-    win.buttons = ["Run"];
-    win.onButtonClick = function() { 
-      eval(win.evalExpr('document.getElementById(\'nodeCMD\').value'));
+    win.buttons = ["Run", "Close"];
+    win.onButtonClick = function(name) {
+      if (name == "Run") { 
+        eval(win.evalExpr('document.getElementById(\'nodeCMD\').value'));
+      } else {
         win.close();
-      }
+      };
+    };
     win.run();
   });
 });
